f3f0ee1fd70e9d337dda350950934f4adbc8c2dc
samxaradzenika <murdoc746@gmail.com>
2023-05-25 16:49:31 +0400

    7-70 transpiling done!

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,45 @@
-import {useState} from 'react';
-import ReactDOM from 'react-dom';
+import * as esbuild from 'esbuild-wasm'
+import {useState, useEffect, useRef} from 'react';
+import ReactDOM from 'react-dom/client';
+
+
 
 const App =()=>{
-
+    const ref = useRef<any>();
     const [input,setInput]= useState('');
     const [code,setCode]=useState('');
 
+    const startService = async () =>{
+        ref.current = await esbuild.startService({
+            worker :true,
+            wasmURL : '/esbuild.wasm'
+        });
+    
+    }
+    useEffect(()=>{
+        startService()
+    },[])
+
+    const onClick =async ()=>{
+        if (!ref.current) {
+            return
+        }
+
+    const result = await ref.current.transform(input,{
+            loader : 'jsx',
+            target : 'es2015'
+        });   
+        
+    setCode(result.code)
+        
+    }
 
     return <div>
-        <textarea onChange={e=>setInput(e.target.value)}></textarea>
+        <textarea value={input} onChange={e=>setInput(e.target.value)}></textarea>
         <div>
-            <button>Submit</button>
+            <button onClick={onClick}>Submit</button>
         </div>
         <pre>{code}</pre>
     </div>;
 }
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-); 
+ReactDOM.createRoot(document.querySelector('#root')).render(<App />);
