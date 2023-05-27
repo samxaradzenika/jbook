2786894961b968a89d1b72b0e64a23d029551999
samxaradzenika <murdoc746@gmail.com>
2023-05-27 18:02:12 +0400

    implemented best practice for bundling

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -9,8 +9,18 @@ export const unpkgPathPlugin = () => {
         console.log('onResolve', args);
         if(args.path === 'index.js') {
             return { path: args.path, namespace: 'a' };
-        }else if (args.path === 'tiny-test-pkg') {
-            return { path: 'https://www.unpkg.com/tiny-test-pkg@1.0.0/index.js', namespace: 'a' };
+        }
+        if(args.path.includes('./')|| args.path.includes('../')){
+            return {namespace : 'a',
+            path : new URL(args.path, 'https://unpkg.com'+ args.resolveDir +'/').href
+        };
+        }
+
+
+
+        return {
+            namespace: 'a',
+            path : `https://unpkg.com/${args.path}`
         }
       });
 
@@ -21,15 +31,19 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('tiny-test-pkg');
-              console.log(message);
+
+            import React, {useState} from 'react';
+            import ReactDOM from 'react-dom';
+              console.log(React, ReactDom);
             `,
           };
         } 
-        const {data} = await axios.get(args.path);
+        const {data,request} = await axios.get(args.path);
+        console.log(request.responseURL);
         return {
             loader: 'jsx',
             contents: data,
+            resolveDir : new URL('./',request.responseURL).pathname
         }
       });
     },
