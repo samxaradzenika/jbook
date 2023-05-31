ec1b594eda800c2a245030e144751b83c87cbb7e
samxaradzenika <murdoc746@gmail.com>
2023-05-31 18:40:16 +0400

    tried out iframe

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const App = () => {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
   useEffect(() => {
@@ -38,6 +38,12 @@ const App = () => {
     // console.log(result);
 
     setCode(result.outputFiles[0].text);
+
+    try{
+      eval(result.outputFiles[0].text);
+    } catch (err){
+      alert (err)
+    }
   };
 
   return (
@@ -50,6 +56,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe sandbox="allow-same-origin" src="/test.html" ></iframe>
     </div>
   );
 };
