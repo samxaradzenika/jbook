8742496e3946810d9b996311c4088c05a6ba8f08
samxaradzenika <murdoc746@gmail.com>
2023-06-07 13:40:06 +0400

    extracted preview

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,11 @@ import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/Code-Editor';
+import Preview from './components/preview';
+
 const App = () => {
   const ref = useRef<any>();
-  const iframe = useRef<any>();
+  const [code,setCode] = useState('')
   const [input, setInput] = useState('');
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -24,7 +26,7 @@ const App = () => {
       return;
     }
 
-    iframe.current.srcdoc= html;
+    
 
     const result = await ref.current.build({
       entryPoints: ['index.js'],
@@ -37,41 +39,18 @@ const App = () => {
       },
     });
 
-    // setCode(result.outputFiles[0].text);
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text,'*')
+    setCode(result.outputFiles[0].text);
   };
 
-    const html =
-    `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener('message',(event)=>{
-            try{
-              eval(event.data);
-            }catch (err){
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color : red;"><h4>Runtime Error</h4>' + err + '</div>';
-              console.error(err);
-            }
-          },false)
-        </script>
-      </body>
-    </html>
-    `
+
   return (
     <div>
       <CodeEditor initialValue="const a=1" onChange = {(value)=> setInput(value)} />
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
+      
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} ></iframe>
+      <Preview code={code} />
     </div>
   );
 };
