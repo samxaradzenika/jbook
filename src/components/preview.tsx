a517eea40660f813bb4b3254c36e8c6c8d0315d0
samxaradzenika <murdoc746@gmail.com>
2023-06-08 01:09:39 +0400

    finished up draggable/resizable components

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -32,7 +32,9 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
 
   useEffect(() => {
     iframe.current.srcdoc = html;
-    iframe.current.contentWindow.postMessage(code, '*');
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, '*');      
+    }, 55);
   }, [code]);
 
   return (
