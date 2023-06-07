fa91731191751c803375c727db58e9d1ad6faac6
samxaradzenika <murdoc746@gmail.com>
2023-06-07 23:00:29 +0400

    styled and displayed properly editor and preview

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,44 +1,50 @@
-import { useEffect, useRef } from "react";
+import './preview.css';
+import { useRef, useEffect } from 'react';
 
-
-interface PreviewProps{
-    code:string;
+interface PreviewProps {
+  code: string;
 }
 
-const html =
-`
-<html>
-  <head></head>
-  <body>
-    <div id="root"></div>
-    <script>
-      window.addEventListener('message',(event)=>{
-        try{
-          eval(event.data);
-        }catch (err){
-          const root = document.querySelector('#root');
-          root.innerHTML = '<div style="color : red;"><h4>Runtime Error</h4>' + err + '</div>';
-          console.error(err);
-        }
-      },false)
-    </script>
-  </body>
-</html>
-`;
+const html = `
+    <html>
+      <head>
+        <style>html { background-color: white; }</style>
+      </head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
-const Preview:React.FC<PreviewProps> = ({code}) =>{
-    const iframe = useRef<any>();
+const Preview: React.FC<PreviewProps> = ({ code }) => {
+  const iframe = useRef<any>();
 
-    useEffect(()=>{
-        iframe.current.srcdoc=html;
-        iframe.current.contentWindow.postMessage(code,'*')
+  useEffect(() => {
+    iframe.current.srcdoc = html;
+    iframe.current.contentWindow.postMessage(code, '*');
+  }, [code]);
 
-    },[code]);
-
-    return <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html}
-    />
-
-
+  return (
+    <div className="preview-wrapper">
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+    </div>
+  );
 };
 
-export default Preview
+export default Preview;
--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,13 @@
-import 'bulmaswatch/superhero/bulmaswatch.min.css'
-import { useState} from 'react';
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
-import CodeEditor from './components/Code-Editor';
-import Preview from './components/preview';
-import bundle from './bundler';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const [code,setCode] = useState('')
-  const [input, setInput] = useState('');
- 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
-
   return (
     <div>
-      <CodeEditor initialValue="const a=1" onChange = {(value)=> setInput(value)} />
-      
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
     </div>
   );
 };
- 
+
 ReactDOM.render(<App />, document.querySelector('#root'));
