2c7bede33f306928827e540f2ea6b2e2a55aa4dc
samxaradzenika <murdoc746@gmail.com>
2023-06-09 01:21:55 +0400

    async error handling

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -18,7 +18,13 @@ const html = `
           root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
           console.error(err);
         };
-          window.addEventListener('message', (event) => {
+
+        window.addEventListener('error',(event)=>{
+          event.preventDefault();
+          handleError(event.error);
+        })
+
+        window.addEventListener('message', (event) => {
             try {
               eval(event.data);
             } catch (err) {
